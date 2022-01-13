4763134d15fee8b505a2ef7bd03837c469777a51
Samuli Jarvinen <frye@github.com>
2022-01-12 21:37:26 -0800

    Using sections to show / hide content.

--- js/quiz.js
+++ js/quiz.js
@@ -7,4 +7,36 @@
  */
 console.log(questions);
 
+var question = questions[0];
+console.log(question.title);
 
+//Get elements
+var scoresButton = document.querySelector('#scores');
+var backButton = document.querySelector('#backButton');
+
+var preSection = document.querySelector('#pregameSection');
+var gameSection = document.querySelector('#gameSection');
+var scoresSection = document.querySelector('#scoresSection');
+
+var init = function() {
+    scoresSection.hidden = true;
+    gameSection.hidden = true;
+}
+
+var scoresClick = function() {
+    preSection.hidden = true;
+    gameSection.hidden = true;
+    scoresSection.hidden = false;
+}
+
+var backButtonClick = function() {
+    preSection.hidden = false;
+    scoresSection.hidden = true;
+}
+
+
+// Event handlers
+scoresButton.addEventListener('click', scoresClick);
+backButton.addEventListener('click', backButtonClick);
+
+init();
